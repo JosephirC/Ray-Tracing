06b0f3630ba66bf025297b02985d37ceb389840e
ABIDA YOUSSEF p2024398 <youssef.abida@etu.univ-lyon1.fr>
2023-11-10 23:05:19 +0000

    fixed syntax error

--- main.cpp
+++ main.cpp
@@ -1804,7 +1804,6 @@ vec3 ShadeOriginal(Ray ray) {
     }
     return vec3(0);
 }
-*/
 
 void mainImage(out vec4 fragColor,in vec2 fragCoord) {
     // From uv which are the pixel coordinates in [0,1], change to [-1,1] and apply aspect ratio
